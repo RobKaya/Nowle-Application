fab08953a487d3327d938d5c2657661502a9396c
samyuktha215 <samyuktha.basam@gmail.com>
2024-01-17 13:52:38 +0100

    changed

--- src/test/java/es/wacoco/nowle/RouteTesting/PatentServiceRouterTest.java
+++ src/test/java/es/wacoco/nowle/RouteTesting/PatentServiceRouterTest.java
@@ -1,11 +1,33 @@
 package es.wacoco.nowle.RouteTesting;
 
 
+import es.wacoco.nowle.Camel.Routes.CamelRoutes;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.test.junit4.CamelTestSupport;
 
-public class PatentServiceRouterTest extends CamelTestSupport {
+import static org.apache.camel.builder.AdviceWith.adviceWith;
 
-    // Inject the Camel context to access routes
+public class PatentServiceRouterTest extends CamelTestSupport {
+@Override
+    public boolean isUseAdviceWith(){
+    return true;
+}
+@Override
+    public RouteBuilder createRouteBuilder()throws Exception{
+    return new CamelRoutes();
+}
+public void TestMockEndpoints()throws Exception{
+    RouteDefinition route= context.getRouteDefinition("");
+    adviceWith(route, context, new AdviceWithRouteBuilder() {
+        @Override
+        public void configure() throws Exception {
+            weaveAddLast().to("mock:");
+        }
+    });
+}
+
 
 }
 
